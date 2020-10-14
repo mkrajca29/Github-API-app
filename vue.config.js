bc5506d77b4d4ee2b6ccd36d85dca3650063ce26
mkrajca29 <mkrajca29@gmail.com>
2020-10-14 11:32:40 +0200

    Config was edited

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   publicPath:
-    process.env.NODE_ENV === "production" ? "/'Github-API-project'/" : "/",
+    process.env.NODE_ENV === "production" ? "/Github-API-project/" : "/",
   chainWebpack: config => {
     config.module
       .rule("eslint")
